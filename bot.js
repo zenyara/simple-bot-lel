90a07b9706a12139be0260abf57ddbcbb1eef2a7
Glitch (simple-bot-lel) <none>
2019-11-09 08:26:03 +0000

    🏌🏙 Updated with Glitch

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ function onMessageHandler(channel, user, msg, self) {
   /*--------------------------------------------------
 			PART TWO - Separate parts
 	---------------------------------------------------*/
-  
+
   // no command found. nothing to return
   if (!_isMatch) {
     // non-command text
@@ -101,27 +101,70 @@ let _command = new Array();
 		3. complex: /email [name@ww.com] [subject]|[msg]|[etc]|[etc]
 */
 
-/*
 _command.push(["!1", "1", "com_1", "!1", "Choose path 1."]);
 _command.push(["!2", "1", "com_2", "!2", "Choose path 2."]);
 _command.push(["!3", "1", "com_3", "!3", "Choose path 3."]);
 _command.push(["!4", "1", "com_4", "!4", "Choose path 4."]);
 
-_command.push(["!buy", "2", "com_buy", "!buy [item name or id]", "Buy an item!"]);
+_command.push([
+  "!buy",
+  "2",
+  "com_buy",
+  "!buy [item name or id]",
+  "Buy an item!"
+]);
+
+_command.push([
+  "!commands",
+  "1",
+  "com_commands",
+  "!commands",
+  "View all commands."
+]);
 
 _command.push(["!d20", "1", "com_d20", "!d20", "Roll a 20-sided die."]);
 
-_command.push(["!deletenote", "2", "com_deletenote", "!deletenote [id]", "Delete a note."]);
+_command.push([
+  "!deletenote",
+  "2",
+  "com_deletenote",
+  "!deletenote [id]",
+  "Delete a note."
+]);
 
-_command.push(["!deleteplayer", "2", "com_deleteplayer", "!deleteplayer [name or id or default(self)]", "Delete a player (self-only if NOT admin)."]);
+_command.push([
+  "!deleteplayer",
+  "2",
+  "com_deleteplayer",
+  "!deleteplayer [name or id or default(self)]",
+  "Delete a player (self-only if NOT admin)."
+]);
 
 _command.push(["!dice", "1", "com_dice", "!dice", "Roll a pair of dice."]);
 
-_command.push(["!join", "1", "com_play", "!join or !play", "Join the next available round."]);
+_command.push([
+  "!join",
+  "1",
+  "com_play",
+  "!join or !play",
+  "Join the next available round."
+]);
 
-_command.push(["!give", "2", "com_give", "!give [target] [item id]", "Give another player an item."]);
+_command.push([
+  "!give",
+  "2",
+  "com_give",
+  "!give [target] [item id]",
+  "Give another player an item."
+]);
 
-_command.push(["!gold", "2", "com_gold", "!give [target] [amount]", "Give another player some gold."]);
+_command.push([
+  "!gold",
+  "2",
+  "com_gold",
+  "!give [target] [amount]",
+  "Give another player some gold."
+]);
 
 _command.push(["!me", "1", "com_stats", "!me or !stats", "View your stats."]);
 
@@ -129,25 +172,71 @@ _command.push(["!note", "2", "com_note", "!note [message]", "Create a note."]);
 
 _command.push(["!notes", "1", "com_notes", "!notes", "View notes list."]);
 
-_command.push(["!play", "1", "com_play", "!join or !play", "Join the next available round."]);
+_command.push([
+  "!play",
+  "1",
+  "com_play",
+  "!join or !play",
+  "Join the next available round."
+]);
 
-_command.push(["!player", "2", "com_player", "!player [name or id]", "View target player's stats."]);
+_command.push([
+  "!player",
+  "2",
+  "com_player",
+  "!player [name or id]",
+  "View target player's stats."
+]);
 
-_command.push(["!players", "1", "com_players", "!players", "View the players list."]);
+_command.push([
+  "!players",
+  "1",
+  "com_players",
+  "!players",
+  "View the players list."
+]);
 
-_command.push(["!rank", "1", "com_rank", "!rank", "View your leaderboard rank."]);
+_command.push([
+  "!rank",
+  "1",
+  "com_rank",
+  "!rank",
+  "View your leaderboard rank."
+]);
 
-_command.push(["!readnote", "2", "com_readnote", "!readnote [id]", "Read a note."]);
+_command.push([
+  "!readnote",
+  "2",
+  "com_readnote",
+  "!readnote [id]",
+  "Read a note."
+]);
 
-_command.push(["!shop", "2", "com_shop", "!shop [armor, pets, potions, weapons]", "View the shop!"]);
+_command.push([
+  "!shop",
+  "2",
+  "com_shop",
+  "!shop [armor, pets, potions, weapons]",
+  "View the shop!"
+]);
 
-_command.push(["!stats", "1", "com_stats", "!me or !stats", "View your stats."]);
+_command.push([
+  "!stats",
+  "1",
+  "com_stats",
+  "!me or !stats",
+  "View your stats."
+]);
 
 _command.push(["!text", "2", "com_text", "!text", "Send OP a text message."]);
 
-_command.push(["!uptime, "1", "com_uptime", "!uptime, "Display this channel's uptime."]);
-
-*/
+_command.push([
+  "!uptime",
+  "1",
+  "com_uptime",
+  "!uptime",
+  "Display this channel's uptime."
+]);
 
 _command.push([
   "!test",
@@ -159,13 +248,27 @@ _command.push([
 
 let _commands = _command.length;
 
+/* use this to output the command list (for txt file)
+for (var i = 0; i < _commands; i++) {
+  console.log(_command[i][0] + ": " + _command[i][4]);
+}*/
+
 /*   ==============  ==============    
 
   START RETURNED CHAT STRING FUNCTIONS
 
       ============== ==============  */
 
-// !d20 command is issued
+function com_commands() {
+  var _cid = arguments[0]; //command id
+  var _cmd = _command[_cid][0]; //command name;
+  var _fn = _command[_cid][2]; //fn name;
+  client.say(
+    _chan,
+    `There are ${_commands} commands. View them here: javascript:void(0)`
+  );
+}
+
 function com_d20() {
   //var _chan = arguments[0];
   const sides = 20;
@@ -176,7 +279,6 @@ function com_d20() {
   );
 }
 
-// "!uptime"
 function com_uptime() {
   client.say(_chan, `Not sure what the uptime (command) is atm.`);
 }
