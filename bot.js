fcbb0ec64ebdb169a2bd841d27c0b185d523d7da
Glitch (simple-bot-lel) <none>
2019-11-19 13:45:04 +0000

    👣🚥 Updated with Glitch

--- bot.js
+++ bot.js
@@ -1,3 +1,54 @@
+// init project
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+//Whenever someone connects this gets executed
+io.on("connection", function(socket) {
+  console.log("A user connected");
+  console.log("A user connected");
+
+  //Send a message after a timeout of 4seconds
+  setTimeout(function() {
+    socket.send("Sent a message 4 seconds after connection!");
+  }, 4000);
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on("disconnect", function() {
+    console.log("A user disconnected");
+  });
+});
+
+// listen for requests :)
+const listener = http.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+/*==================================================
+  ==================================================
+                (ABOVE)
+            UE4 HOOK CODE
+            
+            
+            
+            
+            
+            
+            TWITCH SOCKET CODE
+              (BELOW)
+
+  ==================================================
+  ==================================================*/
+
 const tmi = require("tmi.js");
 const mycoms = require("./mycoms.js");
 const mydb = require("./mydb.js");
